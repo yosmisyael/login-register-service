2d007a848922f6f1125e9ad9e638aac9a2e24c6f
yosmisyael <yosmisyael7308@gmail.com>
2025-01-08 13:31:40 +0700

    feat: created migration for users and sessions tables

---
+++ db/migrations/20250108052149_sessions_table_migration.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class SessionsTableMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('sessions', [
+            'id' => false,
+            'primary_key' => ['id'],
+        ]);
+
+        $table->addColumn('id', 'string', [
+            'null' => false,
+        ])
+            ->addColumn('user_id', 'integer', [
+            'null' => false,
+            'signed' => false,
+            ])
+            ->addForeignKey('user_id', 'users', 'id', [
+                'constraint' => 'fk_users.id_sessions_user_id',
+                'delete' => 'CASCADE',
+                'update' => 'CASCADE'
+            ])
+            ->create();
+    }
+}
