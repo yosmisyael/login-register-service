1f99f5d23ca83c6a3cfe487b3efac2f7ce411213
yosmisyael <yosmisyael7308@gmail.com>
2025-01-08 13:30:49 +0700

    feat: added phinx init config

---
+++ phinx.php
@@ -0,0 +1,45 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => $_ENV['DB_CONNECTION'],
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_NAME'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => 'utf8',
+        ],
+        'development' => [
+            'adapter' => $_ENV['DB_CONNECTION'],
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_NAME_TEST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASSWORD'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => $_ENV['DB_CONNECTION'],
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_NAME_TEST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASSWORD'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
