89c995085ff325a0a0967373327002a57ca8b916
yosmisyael <yosmisyael7308@gmail.com>
2025-01-08 12:14:28 +0700

    feat: moved db credentials to env file

--- app/Config/DatabaseConfig.php
+++ app/Config/DatabaseConfig.php
@@ -1,17 +1,20 @@
-<?php 
+<?php
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
+$dotenv->load();
 
 function getDatabaseConfig(): array {
     return [
         "database" => [
             "test" => [
-                "url" => "mysql:host=127.0.0.1:3306;dbname=login_management_test",
-                "username" => "user",
-                "password" => "user"
+                "url" => $_ENV['DB_CONNECTION'] . ":host=" . $_ENV['DB_HOST'] . ":" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME_TEST'],
+                "username" => $_ENV['DB_USER'],
+                "password" => $_ENV['DB_PASSWORD']
             ],
             "prod" => [
-                "url" => "mysql:host=127.0.0.1:3306;dbname=login_management",
-                "username" => "user",
-                "password" => "user"
+                "url" => $_ENV['DB_CONNECTION'] . ":host=" . $_ENV['DB_HOST'] . ":" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME_TEST'],
+                "username" => $_ENV['DB_USER'],
+                "password" => $_ENV['DB_PASSWORD']
             ]
         ]
     ];
